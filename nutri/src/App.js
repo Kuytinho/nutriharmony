4150840795e2a05046bdc10557d4a9696bf0727d
Lucas Carvalho <drigerm@hotmail.com>
2023-06-22 12:36:56 -0300

    mui no header

--- nutri/src/App.js
+++ nutri/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+// import './App.css';
 import React from "react";
 import { Route, Routes } from 'react-router-dom';
 import Home from './routes/Home';
@@ -7,17 +7,28 @@ import Blog from './routes/Blog';
 import Consultorias from './routes/Consultorias';
 import Programas from './routes/Programas';
 import Galeria from './routes/Galeria';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#00ff00' // Defina o código da cor verde desejada aqui
+    }
+  }
+});
 
 function App() {
   return (
-  <Routes>
-    <Route exact path="/" element={ <Home /> }/>
-    <Route exact path="/agendamento" element={ <Agendamento /> }/>
-    <Route exact path="/blog" element={ <Blog /> }/>
-    <Route exact path="/consultorias" element={ <Consultorias /> }/>
-    <Route exact path="/programas" element={ <Programas /> }/>
-    <Route exact path="/galeria" element={ <Galeria /> }/>
-  </Routes>
+    <ThemeProvider theme={theme}> 
+      <Routes>
+        <Route exact path="/" element={ <Home /> }/>
+        <Route exact path="/agendamento" element={ <Agendamento /> }/>
+        <Route exact path="/blog" element={ <Blog /> }/>
+        <Route exact path="/consultorias" element={ <Consultorias /> }/>
+        <Route exact path="/programas" element={ <Programas /> }/>
+        <Route exact path="/galeria" element={ <Galeria /> }/>
+      </Routes>
+  </ThemeProvider>
   );
 }
 
--- nutri/src/routes/components/header.js
+++ nutri/src/routes/components/header.js
@@ -1,47 +1,46 @@
 import { Link } from "react-router-dom";
-import React from 'react'
+import { AppBar, Toolbar, Drawer, List, ListItem, ListItemText, IconButton, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Header() {
-    return(
-        <div className="links">
-        <Link
-          to="/"
-          className="App-link "
-          >
-            Home
-        </Link>
-        <Link
-          to="/consultorias"
-          className="App-link "
-          >
-            Consultorias
-        </Link>
-        <Link
-          to="/programas"
-          className="App-link "
-          >
-            Programas
-        </Link>
-        <Link
-          to="/blog"
-          className="App-link "
-          >
-            Blog
-        </Link>
-        <Link
-          to="/galeria"
-          className="App-link "
-          >
-            Galeria
-        </Link>
-        <Link
-          to="/agendamento"
-          className="App-link "
-          >
-            Marcar Consulta
-        </Link>
-       </div>
-    )
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu" onClick={() => setIsDrawerOpen(true)}>
+          <MenuIcon />
+        </IconButton>
+        <Drawer anchor="left" open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
+          <div style={{ backgroundColor: '#008000', height: '100%' }}>
+            <List sx={{ backgroundColor: '#008000',  display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', overflowY: 'auto' }}>
+              <ListItem button component={Link} to="/">
+                <ListItemText primaryTypographyProps={{ color: 'red' }} primary="Home" />
+              </ListItem>
+              <ListItem button component={Link} to="/consultorias">
+                <ListItemText primaryTypographyProps={{ color: 'red' }} primary="Consultorias" />
+              </ListItem>
+              <ListItem button component={Link} to="/programas">
+                <ListItemText primaryTypographyProps={{ color: 'red' }} primary="Programas" />
+              </ListItem>
+              <ListItem button component={Link} to="/blog">
+                <ListItemText primaryTypographyProps={{ color: 'red' }} primary="Blog" />
+              </ListItem>
+              <ListItem button component={Link} to="/galeria">
+                <ListItemText primaryTypographyProps={{ color: 'red' }} primary="Galeria" />
+              </ListItem>
+              <ListItem button component={Link} to="/agendamento">
+                <ListItemText primaryTypographyProps={{ color: 'red' }} primary="Marcar Consulta" />
+              </ListItem>
+            </List>
+          </div>
+        </Drawer>
+        <Typography variant="h6" color="inherit">Nutri</Typography>
+        <Typography variant="h6" color="secondary">Harmony</Typography>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Header;
